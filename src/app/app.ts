9cfd5b8dc6beb2abdf715f23b9824c4542a1d3f4
igorbeltrao <igorbeltrao96@gmail.com>
2025-09-20 10:34:33 -0300

    Ajuste de pastas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from '../shared/components/navbar/navbar';
+import { Navbar } from '../app/shared/components/navbar/navbar';
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
